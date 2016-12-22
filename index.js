b0a824cae5cdda3c2c4872099ba81f1648aaa3d9
lukeandersen <luke@lawebdesign.com.au>
2016-12-22 14:36:29 +1100

    Move index route to last position

--- index.js
+++ index.js
@@ -56,17 +56,17 @@ app.use('/public', express.static(path.join(__dirname, '/public')));
 var mountPath = process.env.PARSE_MOUNT || '/parse';
 app.use(mountPath, api);
 
-// Parse Server plays nicely with the rest of your web routes
-app.use('/', dashboard);
-// app.get('/', function(req, res) {
-//     res.status(200).send('I dream of being a website.  Welcome to your parse server.');
-// });
-
 // There will be a test page available on the /test path of your server url
 // Remove this before launching your app
 app.get('/test', function(req, res) {
     res.sendFile(path.join(__dirname, '/public/test.html'));
 });
+
+// Parse Server plays nicely with the rest of your web routes
+app.use('/', dashboard);
+// app.get('/', function(req, res) {
+//     res.status(200).send('I dream of being a website.  Welcome to your parse server.');
+// });
 
 var port = process.env.PORT || 1337;
 var httpServer = require('http').createServer(app);
