325524c8be8d53bfc60bc8a388764e0d1352e580
lukeandersen <luke@lawebdesign.com.au>
2017-01-16 15:22:41 +1100

    Add validation example

--- cloud/main.js
+++ cloud/main.js
@@ -1,4 +1,12 @@
-
+// Cloud function
 Parse.Cloud.define('hello', function(req, res) {
-  res.success('Hi');
+    res.success('Hi');
+});
+
+// Class validation
+Parse.Cloud.beforeSave('GameScore', function(req, res) {
+    if (!req.object.get('playerName')) {
+        res.error('playerName is required');
+    }
+    res.success();
 });
